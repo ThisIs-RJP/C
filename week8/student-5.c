9b9cdb36f3a4821bb1bf2b26642b10c84431d795
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-10-31 19:09:02 +0000

    Add files via upload

---
+++ week8/student-5.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct studentInfo {
+    char *name;
+    int id;
+    float avgScore;
+};
+
+void output(struct studentInfo data) {
+    printf("Students name is %s\n", data.name);
+    printf("Students ID is %d\n", data.id);
+    printf("Average Score is %.2f%%\n", data.avgScore);
+}
+
+int main(int argc, char* argv[]) {
+
+    int k = 1;
+    int index;
+    struct studentInfo maxMax;
+    float max = 100.00;
+    float newMax = 0;
+    int arrayIndex = 0;
+    char name[100];
+    float averageScores = 0;
+    float average = 0;
+
+    for (int i = 1; i < argc; i = i + 8) {
+        index = i + 2;
+        for (int j = 0; j < 6; j = j + 1) {
+            average = average + atof(argv[index]);
+            index = index + 1;
+        }
+
+        struct studentInfo data = {argv[i], atoi(argv[i + 1]), (average/6)};
+        if (i == 1) {
+            struct studentInfo maxMax = {argv[i], atoi(argv[i + 1]), (average/6)};
+        }
+
+        if ((average / 6) < max) {
+            strcpy(name, argv[i]);
+        }
+        if ((average / 6) > newMax) {
+            struct studentInfo maxMax = {argv[i], atoi(argv[i + 1]), (average/6)};
+            newMax = (average / 6);
+        }
+        averageScores = averageScores + (average/6);
+        average = 0;
+        k = 1;
+    }
+    output(maxMax);
+    printf("\n------\n\n");
+    printf("Average Score of all students is: %.2f%%\n", (averageScores / (argc / 8)));
+    printf("The student that scored the lowest is %s\n", name);
+    return 0;
+}
